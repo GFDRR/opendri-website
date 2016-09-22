c48f5a63824c1709a238fc1629e3a54ce6e4ca13
Diego Diestra <ddiestra@trellon.com>
2016-09-22 11:04:02 -0500

    Moving to 301 modules

--- wp-content/plugins/disable-login-autocomplete/disable-login-autocomplete.php
+++ wp-content/plugins/disable-login-autocomplete/disable-login-autocomplete.php
@@ -31,23 +31,3 @@ function wp_ois_custom_nocache_headers($headers) {
   $headers['Cache-Control'] = 'no-cache, no-store';
   return $headers;
 }
-
-add_action('init', 'wp_ois_custom_rewrite_basic');
-function wp_ois_custom_rewrite_basic() {
-  add_rewrite_rule('community-mapping-guide', 'http://gfdrr.github.io/community-mapping/', 'top');
-  add_rewrite_rule('resources', 'http://gfdrr.github.io/resource/', 'top');
-}
-
-function wp_ois_custom_plugin_activate() {
-  wp_ois_custom_rewrite_basic();
-  flush_rewrite_rules();
- }
-
- function wp_ois_custom_plugin_deactivate() {
-  flush_rewrite_rules();
- }
-
- register_activation_hook(__FILE__, 'wp_ois_custom_plugin_activate');
- //register deactivation function
- register_deactivation_hook(__FILE__, 'wp_ois_custom_plugin_deactivate');
- //add rewrite rules in case another plugin flushes rules
