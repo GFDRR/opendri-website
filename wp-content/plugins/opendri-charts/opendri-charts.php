a25e9b53d0b149dbff9fd8893fc6d7799575320d
vectorsize <vectorsize@gmail.com>
2017-07-07 15:19:33 +0200

    add loaders for the rest of the charts and update bundle

--- wp-content/plugins/opendri-charts/opendri-charts.php
+++ wp-content/plugins/opendri-charts/opendri-charts.php
@@ -5,42 +5,107 @@
  * Author: Vizzuality
  * Author URI: http://vizzuality.com
  */
+$ODRI_API_SEVER = "http://54.224.10.82/api/v1";
+$ODRI_MOCK_DATA = "http://localhost:8080/mocks/histogram-features-hti.json";
 
 function compare_map( $atts ) {
+  global $ODRI_API_SEVER;
+  global $ODRI_MOCK_DATA;
   ob_start(); ?>
   <div id="compare-map"></div>
   <script>
-    function compareMap(settings) {
-      ODRI.compareMap('#compare-map', {
-        width: '100%',
-        height: '500px',
-        settings: settings
-      });
-    }
-    var settings = <?php echo json_encode($atts) ?>;
-    settings.iframe_base_url = 'http://localhost:3000';
-    if (settings.polygon === undefined) {
-      var country = settings.country.toUpperCase() || 'HTI';
-      fetch('http://54.224.10.82/api/v1/meta/country_polyline/' + country)
-        .then(function(response) {
-          return response.text();
-        })
-        .then(function(polygon) {
-          settings.polygon = polygon;
-          compareMap(settings);
+    (function() {
+      function compareMap(settings) {
+        ODRI.compareMap('#compare-map', {
+          width: '100%',
+          height: '500px',
+          settings: settings
         });
-    } else {
-      compareMap(settings);
-    }
+      }
+      var settings = <?php echo json_encode($atts) ?>;
+      settings.iframe_base_url = 'http://localhost:3000';
+      if (settings.polygon === undefined) {
+        var country = settings.country.toUpperCase() || 'HTI';
+        fetch('<?php echo $ODRI_API_SEVER; ?>/meta/country_polyline/' + country)
+          .then(function(response) {
+            return response.text();
+          })
+          .then(function(polygon) {
+            settings.polygon = polygon;
+            compareMap(settings);
+          });
+      } else {
+        compareMap(settings);
+      }
+    })()
+  </script>
+  <?php return ob_get_clean();
+}
+
+function activity_chart( $atts ) {
+  global $ODRI_API_SEVER;
+  global $ODRI_MOCK_DATA;
+  ob_start(); ?>
+  <div id="activity-chart"></div>
+  <script>
+  (function() {
+    var settings = <?php echo json_encode($atts) ?>;
+    var country = settings.country.toUpperCase() || 'HTI';
+    // fetch('<?php echo $ODRI_API_SEVER ?>/stats/all/country/' + country)
+    fetch('<?php echo $ODRI_MOCK_DATA ?>')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        ODRI.activity('#activity-chart', {
+          width: '100%',
+          height: '500px',
+          data: data,
+          range: [settings.date_from, settings.date_to]
+        })
+      });
+  })()
+  </script>
+  <?php return ob_get_clean();
+}
+
+
+function contrbutor_chart( $atts ) {
+  global $ODRI_API_SEVER;
+  global $ODRI_MOCK_DATA;
+  ob_start(); ?>
+  <div id="contributor-chart" style="width: 50%"></div>
+  <script>
+  (function() {
+    var settings = <?php echo json_encode($atts) ?>;
+    var country = settings.country.toUpperCase() || 'HTI';
+    // fetch('<?php echo $ODRI_API_SEVER ?>/stats/all/country/' + country)
+    fetch('<?php echo $ODRI_MOCK_DATA ?>')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        ODRI.contributors('#contributor-chart', {
+          width: '100%',
+          height: '500px',
+          data: data,
+          range: [settings.date_from, settings.date_to]
+        })
+      });
+  })()
   </script>
   <?php return ob_get_clean();
 }
 
 add_shortcode( 'opendri_charts_compare_map', 'compare_map' );
-
+add_shortcode( 'opendri_charts_activity', 'activity_chart' );
+add_shortcode( 'opendri_charts_contributors', 'contrbutor_chart' );
 
 function opendri_charts_script() {
- wp_register_script('opendri_charts_bundle', plugins_url('scripts/bundle.js', __FILE__) );
- wp_enqueue_script('opendri_charts_bundle');
+   wp_register_script('opendri_charts_bundle', plugins_url('scripts/bundle.js', __FILE__) );
+   wp_enqueue_script('opendri_charts_bundle');
+   wp_register_style('opendri_charts_styles', plugins_url('styles/styles.css', __FILE__) );
+   wp_enqueue_style('opendri_charts_styles');
 }
+
 add_action( 'wp_enqueue_scripts', 'opendri_charts_script' );
